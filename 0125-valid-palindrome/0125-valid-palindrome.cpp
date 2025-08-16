766be5369dc329ed8a45d60dc00a22380bc08cbd
Prakarsh Singh <108565574+PrakarshSingh5@users.noreply.github.com>
2025-08-16 22:44:44 +0530

    Time: 0 ms (100%), Space: 10 MB (42.98%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,27 @@
+
+
+class Solution {
+public:
+    bool isPalindrome(std::string s) {
+        int i = 0, j = s.size() - 1;
+        while(i < j) { 
+            
+            while (i < j && !std::isalnum(s[i])) {
+                i++;
+            }
+            
+        
+            while (i < j && !std::isalnum(s[j])) {
+                j--;
+            }
+
+            if (std::tolower(s[i]) != std::tolower(s[j])) {
+                return false;
+            }
+            
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
