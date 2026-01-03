63bdffc5c3b79176d1ee959d10100d657f19afd4
Prakarsh Singh <108565574+PrakarshSingh5@users.noreply.github.com>
2026-01-04 02:38:57 +0530

    Time: 699 ms (32.99%), Space: 184.4 MB (40.89%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,32 +1,64 @@
 class Solution {
+
 public:
-    int findMaximumXOR(vector<int>& nums) {
-        int ans = 0;
-        int mask = 0;
-
-        for (int bit = 31; bit >= 0; bit--) {
-            mask = mask | (1 << bit);
-            unordered_set<int> s;
-
-            for (int x : nums) {
-                s.insert(x & mask);
-            }
-
-            int candidate = ans | (1 << bit);
-
-            bool possible = false;
-            for (int p : s) {
-                if (s.count(p ^ candidate)) {
-                    possible = true;
-                    break;
+    struct trieNode {
+        trieNode* left;
+        trieNode* right;
+    };
+    void insert(trieNode* root, int& val) {
+        trieNode* pCrawl = root;
+        for (int i = 31; i >= 0; i--) {
+            int i_bit = (val >> i) & 1;
+            if (i_bit == 0) {
+                if (pCrawl->left == NULL) {
+                    pCrawl->left = new trieNode();
                 }
-            }
+                pCrawl = pCrawl->left;
 
-            if (possible) {
-                ans = candidate;
+            } else {
+                if (pCrawl->right == NULL) {
+                    pCrawl->right = new trieNode();
+                }
+                pCrawl = pCrawl->right;
             }
         }
+    }
+    int maxXor(trieNode* root, int &val){
+        int maxi=0;
+        trieNode* temp=root;
+        for(int i=31;i>=0;i--){
+            int j=(val>>i) & 1;
+            if(j==1){
+                if(temp->left != NULL){
+                    maxi+=pow(2, i);
+                    temp=temp->left;
+                }else {
+                    temp=temp->right;
+                }
+                
+            }else {
+                if(temp->right!=NULL){
+                    maxi+=pow(2, i);
+                    temp=temp->right;
+                }else {
+                    temp=temp->left;
+                }
+            }
+        }
+        return maxi;
+    }
+    int findMaximumXOR(vector<int>& nums) {
+        int n = nums.size();
+        trieNode* root = new trieNode();
 
-        return ans;
+        for (auto it : nums) {
+            insert(root, it);
+        }
+        int max_res = 0;
+        for (int i = 0; i < n; i++) {
+            int temp = maxXor(root, nums[i]);
+            max_res = max(temp, max_res);
+        }
+        return max_res;
     }
 };
